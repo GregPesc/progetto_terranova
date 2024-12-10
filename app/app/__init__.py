c607f4073da889b148a1d49dbb55b92672e6a0c4
GregPesc <gregorio.pescucci@polomanettiporciatti.edu.it>
2024-12-10 16:17:53 +0100

    Local CSS and JS dependencies. Jinja configuration.

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,3 +1,4 @@
+import jinja2
 from flask import Flask
 
 from app.config import Config
@@ -6,6 +7,10 @@ from app.config import Config
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    app.jinja_env.autoescape = jinja2.select_autoescape()
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
 
     from app.main.routes import main
 
