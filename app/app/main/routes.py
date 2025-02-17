2ae14e0228a1e0b1a1879e12dd8e0a8852ecc4b0
gregpesc <gregpesc>
2025-02-17 09:41:48 +0100

    da testare

--- app/app/main/routes.py
+++ app/app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file
 
 main = Blueprint("main", __name__)
 
@@ -6,3 +6,13 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html", title="Applicazione")
+
+
+@main.route("/manifest.json")
+def serve_manifest():
+    return send_file("manifest.json", mimetype="application/manifest+json")
+
+
+@main.route("/sw.js")
+def serve_sw():
+    return send_file("sw.js", mimetype="application/javascript")
